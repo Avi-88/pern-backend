01615fe2aea7d49dcad463154f45d5d832a7d6c3
Avi <avishkar.yt@gmail.com>
2024-01-14 11:25:16 +0530

    added healthcheck route

--- server.js
+++ server.js
@@ -4,24 +4,21 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import UserRouter from "./routes/user.js";
 import PostRouter from "./routes/post.js";
-import CompanyRouter from './routes/company.js'
+import CompanyRouter from "./routes/company.js";
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use(
-  cors({
-    origin: ["http://localhost:3000"],
-    methods: ["POST", "GET", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
+app.use("/healthCheck" , (req, res) => {
+  res.status(200).json("The server is up and running :)");
+})
 app.use("/api/user", UserRouter);
 app.use("/api/post", PostRouter);
-app.use("/api/company",CompanyRouter)
+app.use("/api/company", CompanyRouter);
 
 app.listen(5000, async () => {
   console.log("server is running on port 5000");
